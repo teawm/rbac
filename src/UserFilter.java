62c7124c8b57f537c44d4c935fd35b4230ab6177
Малышев <hukullikot@gmail.com>
2026-02-23 12:06:53 +0700

    Step 2.1

---
+++ src/UserFilter.java
@@ -0,0 +1,18 @@
+import java.util.function.Predicate;
+
+@FunctionalInterface
+public interface UserFilter {
+    boolean test(User user);
+
+    default UserFilter and(UserFilter other) {
+        return user -> this.test(user) && other.test(user);
+    }
+
+    default UserFilter or(UserFilter other) {
+        return user -> this.test(user) || other.test(user);
+    }
+
+    default UserFilter negate() {
+        return user -> !this.test(user);
+    }
+}
